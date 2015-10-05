d1e407aed98fff9cb99cbd5609d524ed0495e0df
pedesen <patrick.dehn@camunda.com>
2015-10-05 11:32:42 +0200

    chore: simplify stuff

--- bpmn-jsonator.js
+++ bpmn-jsonator.js
@@ -1,20 +1,19 @@
 var jsonfile = require('jsonfile');
-var util = require('util');
 var filter = require('lodash.filter');
 var pluck = require('lodash.pluck');
 
-if (!process.argv[2] || !process.argv[3]) {
+var args = process.argv;
+
+if (!args[2] || !args[3]) {
   console.error('\nUsage: node bpmn-jsonator.js /path/to/bpmn.json element');
   return;
 }
 
-var file = process.argv[2];
-var elementName = process.argv[3];
+var file = args[2];
+var elementName = args[3];
 
 jsonfile.readFile(file, function(err, obj) {
-  var types = obj.types;
-
-  var elements = filter(types, function(type) {
+  var elements = filter(obj.types, function(type) {
     if (type.superClass) {
       return type.superClass.indexOf(elementName) !== -1;
     } else {
